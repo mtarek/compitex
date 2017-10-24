653f6e9bf6a400ec54983b7e6b9e85a65377265d
Mohamed Tarek El-Haddad <mtarek16@gmail.com>
2017-10-24 12:17:09 -0500

    Added licensing information

--- util.c
+++ util.c
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *  
+ *                    ___                _ _____   __  __
+ *                   / __|___ _ __  _ __(_)_   _|__\ \/ /
+ *                  | (__/ _ \ '  \| '_ \ | | |/ -_)>  < 
+ *                   \___\___/_|_|_| .__/_| |_|\___/_/\_\
+ *                                 |_|                  
+ *
+ * Copyright (C) 2012 - 2017, Mohamed Tarek El-Haddad <mtarek16@gmail.com>.
+ *
+ * This software is licensed as described in the LICENSE file, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the COPYING file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
 #include <string.h>
 #include <stdarg.h>
 #include "util.h"
